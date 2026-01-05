f2d304b8905f5492850dfc506fb3bc39f9f7fd9c
OluStayhired <olu@prelo.io>
2026-01-05 14:57:20 +0000

    added emotion to the info

--- src/pages/LandingPageDev.tsx
+++ src/pages/LandingPageDev.tsx
@@ -352,7 +352,7 @@ const handleLoginClick = () => {
 
                   <span className="hidden sm:inline font-normal">
                     We're a closed community of career professionals solving caregiving challenges 
-                    <br/> with battle-tested systems that will save you 12 hours every week.
+                    <br/> with battle-tested systems that will save you 12 hours every week! 😲
                   </span> 
          </p>
               </span>
