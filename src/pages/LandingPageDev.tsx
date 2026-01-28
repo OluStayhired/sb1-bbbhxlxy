bed9f87d4db841d7f3e75b4f44a4a36afa8d669d
OluStayhired <olu@prelo.io>
2026-01-28 15:53:19 +0000

    changed documents to affairs in order

--- src/pages/LandingPageDev.tsx
+++ src/pages/LandingPageDev.tsx
@@ -771,11 +771,11 @@ const getSessionId = (): string => {
                 
        <p className="block text-sm font-normal sm:text-xl sm:font-normal text-gray-600 leading-tight mt-1 sm:mt-3">
          
-             <span className="sm:hidden font-normal">Get legal and financial documents in place for mom and dad before a health crisis hits.</span> 
+             <span className="sm:hidden font-normal">Get legal and financial affairs in order for mom and dad before a health crisis hits.</span> 
 
           <span className="hidden text-2xl sm:inline font-normal">
 
-          Get legal and financial documents in place for mom and dad before a health crisis hits.
+          Get legal and financial affairs in order for mom and dad before a health crisis hits.
             
           </span>
          
