dd9537c7335eae06e57d7977b34ff753bb95ae3a
null <null>
2026-08-24 13:26:19 +0100

    Download Checklist Fixed

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -59,21 +59,16 @@ export function PageFooter({ onOpenOnboardingModal }: PageFooterProps) {
                   </li>
                 </ul>
               </div>
-              {/*Start Resources*/}
-                <div>
+              <div>
                 <h3 className="text-xl mb-4 font-bold text-gray-700 sm:text-xl">resources</h3>
                 <ul className="space-y-2 text-sm text-gray-600">
                   <li>
-                    <Link to="https://app.poetiq.io/elder-care-checklist" className="flex items-center gap-3 hover:text-red-500 transition-colors">
+                    <a href="https://app.poetiq.io/elder-care-checklist" className="flex items-center gap-3 hover:text-red-500 transition-colors">
                         download eldercare checklist
-                    </Link>
+                    </a>
                   </li>
                 </ul>
               </div>
-              {/*End Resources*/}
-            </div>
-          </div>
-        </div>
             </div>
           </div>
         </div>
